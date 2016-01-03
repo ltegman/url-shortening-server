058fba2335987304f51b4442f9e8b18d4ee7f88b
Logan Tegman <ltegman@gmail.com>
2016-01-03 04:44:00 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# URL Shortening Server
+
+Simple URL Shortening server created with Express and Mongo on NodeJS.
+Built as a part of the Free Code Camp Curriculum. It starts generating URLS
+with `0` and increments though 0 - 9, a-z, and A-Z, adding characters as
+necessary.
+
+The project uses ES6 features so it may be necessary to use Node 4.2 or above.
+
+## Get it Running
+
++   Have mongo running and in the mongo shell `use urls` to create the db.
++   Then simply `npm install` and `npm run start`.
+
+## API Usage
+
+Simply send the URL you want to shorten to `/set/:url` and you will get a JSON
+response with the shortened URL. Visiting this URL does a 302 redirect to the
+URL that was shortened.
+
+### Examples
+
+Good Request:
+```
+serverurl.com/set/http://google.com
+```
+
+Good Response:
+```javascript
+{shortURL: 'http://serverurl.com/b'}
+```
+
+Bad Request:
+```
+serverurl.com/set/badtest
+```
+
+Bad Response:
+```javascript
+{error: 'invalid URL'}
+```
+
+## License
+
+The project is licensed under the terms of the MIT license.
---
+++ server.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const validator = require('validator');
+const normalizer = require('normalize-url');
+const alphaInc = require('./utils/alphanumeric-increment');
+
+
+/* SCHEMA
+{
+  shortcut: 'short',
+  url: 'http://path-to-url.com'
+}
+*/
+const db = require('monk')(`${process.env.MONGOLAB_URI || 'localhost'}/urls`);
+const urls = db.get('urls');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(path.resolve(__dirname, 'client')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/:short', (req, res) => {
+  const short = req.params.short;
+  urls.findOne({shortcut: {$eq: short}})
+    .success(doc => {
+      if (doc) {
+        res.redirect(doc.url);
+      } else {
+        res.json({error: 'no record found'});
+      }
+    })
+    .error(err => res.json({error: 'no record found'}));
+});
+
+app.get(/^\/set\/(.+)/, (req, res) => {
+  const url = req.params[0];
+  // return early if URL is invalid
+  if (!validator.isURL(url)) {
+    res.json({error: 'invalid URL'});
+    return;
+  }
+  
+  const normal = normalizer(url); 
+  
+  // If this url has already been shortened, return that
+  urls.find({url: {$eq: normal}})
+    .success(doc => {
+      if (doc.length !== 0) {
+        doc = doc[0];
+        res.json({shortURL: `${req.protocol}://${req.get('host')}/${doc.shortcut}`});
+      } else {
+        // if valid and not found, make a new shortcut url and return
+        urls.find({}, {sort: {_id: -1}, limit: 1})
+          .success(doc => {
+            let nextNum;
+            if (!doc) {
+              nextNum = '0';
+            }
+            doc = doc[0];
+            nextNum = alphaInc.next('' + doc.shortcut);
+            // insert record and return short url
+            urls.insert({shortcut: nextNum, url: normal})
+              .success(doc => {
+                res.json({shortURL: `${req.protocol}://${req.get('host')}/${doc.shortcut}`});
+              })
+              .error(err => {
+                res.json({error: err});
+              });
+          });
+      }
+    });
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`server listening on port ${app.get('port')}`);
+});
